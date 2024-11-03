c2767b74309558e2b841e3f48051d09b2b2e30d3
Samed Bilgili <samedbilgili99@gmail.com>
2024-11-03 17:25:54 +0300

    key attribute added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,8 +9,8 @@ export default function Home() {
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products ({products.length})</h2>
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {products.map((product) => (
-                        <div key={product.id} className="group relative">
+                    {products.map((product, index) => (
+                        <div key={index} className="group relative">
                             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                 <img
                                     alt="image not fount"
